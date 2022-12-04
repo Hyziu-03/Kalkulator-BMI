df8bb8c04c5408c0619c03a24477716da9cd0f3e
Szymon Hyziak <szymon.hyziak@protonmail.com>
2022-12-04 12:00:02 +0100

    Wczytywanie danych z pliku CSV
    Dodałem mechanizm wczytywania danych z przykładowego pliku.  Zmieniłem również kod @patrycjawider, załadowałem dane z pomocą biblioteki Pandas i zmieniłem ich nazwę na anglojęzyczną (taka jest konwencja)

--- wizualizacjadanych.py
+++ wizualizacjadanych.py
@@ -1,14 +1,44 @@
 import seaborn as sns
+import pandas
+
+# WCZYTYWANIE DANYCH
+
+file = open("dane.csv", "r")
+# przykładowy plik dla testów
+
+contents = file.read()
+records_list = contents.split("\n")
+# \n to znak nowej linii
+records_size = len(records_list)
+for i in range(records_size):
+    records_list[i] = records_list[i].split(",")
+# wczytywanie danych z pliku do listy
+
+names = []
+weights = []
+heights = []
+for i in range(records_size):
+    names.append(records_list[i][0])
+    weights.append(records_list[i][1])
+    heights.append(records_list[i][2])
+# przepisywanie danych z jednej listy do trzech
+# każda lista to osobne dane, każdy indeks odpowiada jednej osobie
+# to znaczy names[0], weights[0] i heights[0] odpowiadają tej samej osobie
+
+# WIZUALIZACJA DANYCH
+
 sns.set_theme(style="whitegrid")
 
-ludzie = sns.load_dataset("ludzie")
-
+people = pandas.read_csv("dane.csv")
 
 g = sns.catplot(
-    data=ludzie, kind="bar",
-    x="species", y="wartosc BMI", hue="sex",
+    data=people, kind="bar",
+    x="species", y="wartość BMI", hue="sex",
     errorbar="sd", palette="dark", alpha=.6, height=6
 )
+# o co chodzi, wyskakuje błąd ValueError: Could not interpret input 'species'
 g.despine(left=True)
-g.set_axis_labels("", "Wartosc BMI")
+g.set_axis_labels("", "Wartość BMI")
 g.legend.set_title("")
+
+file.close()
