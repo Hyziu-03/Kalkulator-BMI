c21142ffa6f55a8753deb7110dcd6018a177b8a1
Szymon Hyziak <szymon.hyziak@protonmail.com>
2022-12-05 11:21:31 +0100

    Dodanie kategorii w pliku csv i tworzenie wykresu

--- wizualizacjadanych.py
+++ wizualizacjadanych.py
@@ -1,9 +1,10 @@
 import seaborn as sns
 import pandas
+import matplotlib.pyplot as plt
 
 # WCZYTYWANIE DANYCH
 
-file = open("dane.csv", "r")
+file = open("bmi_output.csv", "r")
 # przykładowy plik dla testów
 
 contents = file.read()
@@ -14,31 +15,36 @@ for i in range(records_size):
     records_list[i] = records_list[i].split(",")
 # wczytywanie danych z pliku do listy
 
+systems = []
 names = []
 weights = []
 heights = []
+bmis = []
+
 for i in range(records_size):
-    names.append(records_list[i][0])
-    weights.append(records_list[i][1])
-    heights.append(records_list[i][2])
+    systems.append(records_list[i][0])
+    names.append(records_list[i][1])
+    weights.append(records_list[i][2])
+    heights.append(records_list[i][3])
+    bmis.append(records_list[i][4])
 # przepisywanie danych z jednej listy do trzech
 # każda lista to osobne dane, każdy indeks odpowiada jednej osobie
-# to znaczy names[0], weights[0] i heights[0] odpowiadają tej samej osobie
+# to znaczy systems[0], names[0], weights[0], heights[0] i bmis[0]...
+# ...odpowiadają tej samej osobie
 
 # WIZUALIZACJA DANYCH
 
 sns.set_theme(style="whitegrid")
 
-people = pandas.read_csv("dane.csv")
+people = pandas.read_csv("bmi_output.csv")
 
 g = sns.catplot(
-    data=people, kind="bar",
-    x="species", y="wartość BMI", hue="sex",
-    errorbar="sd", palette="dark", alpha=.6, height=6
+    data=people, kind="bar", errorbar="sd",
+    x="imie", y="bmi",
+    palette="dark", alpha=.6, height=6
 )
-# o co chodzi, wyskakuje błąd ValueError: Could not interpret input 'species'
 g.despine(left=True)
-g.set_axis_labels("", "Wartość BMI")
-g.legend.set_title("")
+g.set_axis_labels("Osoba", "Wartość BMI")
+plt.show()
 
 file.close()
