650fd950fdf85f647762c3921ed21a818aff5aab
Szymon Hyziak <szymon.hyziak@protonmail.com>
2022-12-05 10:50:42 +0100

    Poprawa kodu dla zgodności z PEP8
    Poza poprawą zmieniłem zapis do pliku uwzględniając jednostki i zmieniając jego typ z tekstowego na csv

--- bmi-input.py
+++ bmi-input.py
@@ -1,31 +1,35 @@
-f = open("bmi_output.txt", "a") #otwiera się plik tekstowy o nazwie bmi_output
+f = open("bmi_output.csv", "a")  # otwiera się plik tekstowy o nazwie bmi_output
 
-def imperialne(): #funkcja dla wartości imperialnych
+
+def imperialne():  # funkcja dla wartości imperialnych
     while True:
-        wzrost_stopy = int(input("Ile mierzysz stóp: ")) #często ludzie pamiętają swój wzrost w danych imperialnych używając stóp i cali
+        pseudonim = input("Podaj swój pseudonim/imię: ")
+        wzrost_stopy = int(input("Ile mierzysz stóp: "))  # często ludzie pamiętają swój wzrost w danych imperialnych używając stóp i cali
         wzrost_cale = float(input("Ile mierzysz cali: "))
         waga_funty = float(input("Podaj swoją wagę w funtach: "))
-        wzrost_imp = wzrost_stopy * 12 + wzrost_cale #funkcja łączy stopy i cale dla ułatwienia obliczeń i późniejszego outputu
+        wzrost_imp = wzrost_stopy * 12 + wzrost_cale  # funkcja łączy stopy i cale dla ułatwienia obliczeń i późniejszego outputu
 
-        bmi = (waga_funty / wzrost_imp ** 2) * 703 #wzór na bmi dla wartości imperialnych
+        bmi = (waga_funty / wzrost_imp ** 2) * 703  # wzór na bmi dla wartości imperialnych
 
         print(f'Twoje BMI wynosi:  {bmi}')
 
-        print(f'Wzrost: {wzrost_imp}in, Waga: {waga_funty}lb, BMI: {bmi}', file=f) #zapisanie wartości do pliku tekstowego
+        print(f'imp,{pseudonim},{wzrost_imp},{waga_funty},{bmi}', file=f)  # zapisanie wartości do pliku tekstowego
 
         kolejna = input("Czy chciałbyś dodać kolejną osobę używając jednostek imperialnych? (tak lub nie): ")
-        if kolejna == 'tak': #jeśli ktoś chce dalej dodawać osoby w tym samym formacie danych to może to robić wpisując tak
+        if kolejna == 'tak':  # jeśli ktoś chce dalej dodawać osoby w tym samym formacie danych to może to robić wpisując tak
             continue
-        if kolejna == 'nie': #jeśli chce zmienić dane na metryczne albo całkowicie opuścić program to wpisuje nie
+        if kolejna == 'nie':  # jeśli chce zmienić dane na metryczne albo całkowicie opuścić program to wpisuje nie
             kolejnamet = input("Czy chciałbyś dodać kolejną osobę używając jednostek metrycznych? (tak lub nie): ")
             if kolejnamet == 'tak':
                 metryczne()
-            elif kolejnamet == 'nie': #po dwukrotnym wpisaniu 'nie' zamyka się plik tekstowy a następnie program
+            elif kolejnamet == 'nie':  # po dwukrotnym wpisaniu 'nie' zamyka się plik tekstowy a następnie program
                 f.close()
                 exit()
 
-def metryczne(): #funkcja dla wartości metrycznych
+
+def metryczne():  # funkcja dla wartości metrycznych
     while True:
+        pseudonim = input("Podaj swój pseudonim/imię: ")
         wzrost = float(input("Podaj swój wzrost w metrach: "))
         waga = float(input("Podaj swoją wagę w kilogramach: "))
 
@@ -33,9 +37,9 @@ def metryczne(): #funkcja dla wartości metrycznych
 
         print(f'Twoje BMI wynosi:  {bmi}')
 
-        print(f'Wzrost: {wzrost}m, Waga: {waga}kg, BMI: {bmi}', file=f) #zapisanie wartości do pliku tekstowego
+        print(f'met,{pseudonim},{wzrost},{waga},{bmi}', file=f)  # zapisanie wartości do pliku tekstowego
 
-        kolejna = input("Czy chciałbyś dodać kolejną osobę używając jednostek metrycznych? (tak lub nie): ") #te pętle działają tak samo jak poprzednie
+        kolejna = input("Czy chciałbyś dodać kolejną osobę używając jednostek metrycznych? (tak lub nie): ")  # te pętle działają tak samo jak poprzednie
         if kolejna == 'tak':
             continue
         if kolejna == 'nie':
@@ -47,7 +51,7 @@ def metryczne(): #funkcja dla wartości metrycznych
                 exit()
 
 
-q = input("Chcesz podawać dane w jednostkach imperialnych czy metrycznych?: ") #pytanie początkowe do użytkownika jakich wartości chciałby używać
+q = input("Chcesz podawać dane w jednostkach imperialnych czy metrycznych?: ")  # pytanie początkowe do użytkownika jakich wartości chciałby używać
 if q == 'imperialnych':
     imperialne()
 if q == 'metrycznych':
