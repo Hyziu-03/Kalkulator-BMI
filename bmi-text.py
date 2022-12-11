94c3de58f47ebb4f6d9984db09a589b1b66f5644
radini5948 <110565134+radini5948@users.noreply.github.com>
2022-12-11 22:15:21 +0100

    Update bmi-text.py

--- bmi-text.py
+++ bmi-text.py
@@ -1,18 +1,17 @@
+import csv
+# zaimportowanie biblioteki csv
+
+
 def main():
     """ funkcja main """
     with open("people.csv", "r+") as file:
-        # otwarcie pliku z danymi
-        people = []
-        i = 0
-        # deklaracja list i zmiennych
+        # otworzenie pliku z danymi
+        reader = csv.DictReader(file)
+        # szczytanie wartości z pliku używając funkcji DictReader(szczytanie zawartości jako słowników)
 
-        for line in file:
-            new_item = line.split(",")
-            people.append(new_item)
-        # szczytanie wartości z pliku, dodanie elementów do listy
-
-        for i in range(len(people)):
-            people_weight = people[i][1]
+        for people in reader:
+            people_name = people["name"]
+            people_weight = people["weight"]
             weight_and_unit = people_weight.split(" ")
             weight = float(weight_and_unit[0])
             weight_unit = weight_and_unit[1]
@@ -20,7 +19,7 @@ def main():
                 weight = pound_to_kg(weight)
             # zamiana z systemu imperialnego(funty) na normalny
 
-            people_height = people[i][2]
+            people_height = people["height"]
             height_and_unit = people_height.split(" ")
             height = float(height_and_unit[0])
             height_unit = height_and_unit[1]
@@ -37,32 +36,30 @@ def main():
             new_bmi = str(f"{bmi:.2f}")
             # ograniczenie cyfr po przecinku do dwóch
 
-            file1 = open("file_to_plot.csv", "a+")
-            file1.write(f"{people[i][0]},{weight:.2f},{height:.2f}\n")
+            file1 = open("bmi.csv", "a+")
+            file1.write(f"met,{people_name},{weight:.2f},{height:.2f},{new_bmi}\n")
             file1.close()
             # otwarcie pliku, wpisanie wartości, które posłużą do wstawienia w wykres, zamknięcie pliku
-
-            file2 = open("bmi.csv", "a+")
-            file2.write(f"{people[i][0]},{new_bmi}\n")
-            file2.close()
-            # otwarcie pliku, wpisanie imion i odpowiadającemu im bmi, zamknięcie pliku
-
-            i += 1
-            # inkrementacja zmiennej i służącej do określenia indeksu w liście
 
 
 def pound_to_kg(pound):
     return float(pound * 0.4535923)
+
+
 """ funkcja konwertująca funty na kilogramy """
 
 
 def inch_to_cm(inch):
     return float(inch * 2.54)
+
+
 """ funkcja konwertująca cale na centymetry """
 
 
 def cm_to_m(cm):
     return float(cm / 100)
+
+
 """ funkcja konwertująca metry na centymetry """
 
 
